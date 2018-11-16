7b54349a3b2fcaffaa108f36da3a3e1f4a9b1079
Sail338 <srihari.shankar1@gmail.com>
2018-11-15 19:38:46 -0800

    finished parsing audio

--- parse/ParseVideo.py
+++ parse/ParseVideo.py
@@ -5,10 +5,15 @@ from apiclient.errors import HttpError
 from google.cloud import storage
 from google.cloud import speech
 from google.cloud.speech import enums
+import os
+import io
 from google.cloud.speech import types
+import subprocess
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 import config
+max_timestamp = 0
+
 class YouTubeDlLogger(object):
     def debug(self, msg):
         pass
@@ -37,16 +42,15 @@ class PreParseNode(object):
 
 
 
-def uploadToGcp(filename):
-    storage_client = storage.Client()
-    bucket_to_use = storage_client.get_bucket('facebookglobalhackathon')
-    blob = bucket_to_use.blob(filename)
-    if not blob.exists():
-        blob.upload_from_filename(filename)
-    #return the file uri
-    uri_base = "gs://facebookglobalhackathon/"
-    uri = uri_base + filename
-    return uri
+
+
+
+
+def extract(input_filename):
+    print(input_filename)
+    
+    command = "ffmpeg -i " + input_filename + " -ab 160k -ac 1 -ar 16000 -vn "  + input_filename[0:len(input_filename) -3] + ".flac"
+    subprocess.call(command, shell=True)
 
 
 
@@ -64,6 +68,58 @@ def parseVideos(svideoLink:list,outputId):
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download(svideoLink)
+
+def transcribe_gcs():
+    global max_timestamp
+    client = speech.SpeechClient()
+    files = sorted(os.listdir('parts/'))
+    json_arr = []
+    for f in files:
+        name = "parts/" + f
+        if('mp3' in name):
+            extract(name)
+            real_name = name[0:len(name)-3] + ".flac"
+            os.remove(name)
+            with io.open(real_name, 'rb') as audio_file:
+        
+                json_ret = {}
+                content = audio_file.read()
+
+                audio = types.RecognitionAudio(content=content)
+                config = types.RecognitionConfig(
+                     encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+                        sample_rate_hertz=16000,
+                        language_code='en-US',
+                    enable_word_time_offsets=True
+                    )
+
+                response = client.recognize(config, audio)
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+                for result in response.results:
+                    alternative = result.alternatives[0]
+        # The first alternative is the most likely one for this portion.
+                    print(u'Transcript: {}'.format(result.alternatives[0].transcript))   
+                    for i in range(len(alternative.words)):
+                         word_info = alternative.words[i]
+                         word = word_info.word
+                        
+                         start_time = word_info.start_time
+                         end_time = word_info.end_time
+                         json_ret['word'] = word
+                         json_ret['start_time'] = start_time.seconds + start_time.nanos * 1e-9 + max_timestamp
+                         json_ret['end_time'] = end_time.seconds + end_time.nanos * 1e-9 + max_timestamp
+                         json_arr.append(json_ret)
+                         if(i == len(alternative.words)-1):
+                             #update max
+                             max_timestamp = json_ret['end_time']
+            os.remove(real_name)
+
+                    
+
+def splitAudio(bigAudio):
+    command =  "ffmpeg -i  " + bigAudio + " -f segment -segment_time 30 -c copy parts/out%03d.mp3"
+    subprocess.call(command,shell=True)
 
 def getVideosGivenPlayList(playListID:str):
     #build youtube client
@@ -83,8 +139,17 @@ def getVideosGivenPlayList(playListID:str):
         youtubeUrl = "https://www.youtube.com/watch?v=" + videoID
         #parse the audio file
         parseVideos([youtubeUrl],videoID)
-        uploadToGcp(videoID + ".mp3")
-        preParsedNodes.append(PreParseNode(youtubeUrl,videoID,title,None))
+        #convert mp3 to flac
+       
+        #uri = uploadToGcp("out.flac")
+        #split the audiio file
+        splitAudio(videoID +".mp3")
+        ret = transcribe_gcs()
+        print(ret)
+        preParsedNodes.append(PreParseNode(youtubeUrl,videoID,title,ret))
     return preParsedNodes
+
+
+
 getVideosGivenPlayList("PLfMspJ0TLR5HRFu2kLh3U4mvStMO8QURm")
 
