550274b330b1d2bee293be9e272c8b199dbde5ba
Sail338 <srihari.shankar1@gmail.com>
2018-11-15 20:45:18 -0800

    yeet that shit

--- parse/ParseVideo.py
+++ parse/ParseVideo.py
@@ -9,10 +9,15 @@ import os
 import io
 from google.cloud.speech import types
 import subprocess
+from threading import Thread,Lock
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 import config
 max_timestamp = 0
+currentReduceList = []
+lock = Lock()
+
+
 
 class YouTubeDlLogger(object):
     def debug(self, msg):
@@ -41,10 +46,12 @@ class PreParseNode(object):
         pass
 
 
-
-
-
-
+def reduce(tup):
+    global currentReduceList
+    lock.acquire()
+    currentReduceList.append(tup)
+    currentReduceList.sort(key=lambda x: x[0])
+    lock.release()
 
 def extract(input_filename):
     print(input_filename)
@@ -52,6 +59,46 @@ def extract(input_filename):
     command = "ffmpeg -i " + input_filename + " -ab 160k -ac 1 -ar 16000 -vn "  + input_filename[0:len(input_filename) -3] + ".flac"
     subprocess.call(command, shell=True)
 
+def performWork(name):
+    client = speech.SpeechClient()
+    json_arr = []
+    extract(name)
+    real_name = name[0:len(name)-3] + ".flac"
+    os.remove(name)
+    with io.open(real_name, 'rb') as audio_file:
+
+        json_ret = {}
+        content = audio_file.read()
+
+        audio = types.RecognitionAudio(content=content)
+        config = types.RecognitionConfig(
+                encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+                sample_rate_hertz=16000,
+                language_code='en-US',
+            enable_word_time_offsets=True
+            )
+
+        response = client.recognize(config, audio)
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+        for result in response.results:
+            alternative = result.alternatives[0]
+    # The first alternative is the most likely one for this portion.
+            #print(u'Transcript: {}'.format(result.alternatives[0].transcript))   
+            for i in range(len(alternative.words)):
+                    word_info = alternative.words[i]
+                    word = word_info.word
+                
+                    start_time = word_info.start_time
+                    end_time = word_info.end_time
+                    json_ret['word'] = word
+                    json_ret['start_time'] = start_time.seconds + start_time.nanos * 1e-9
+                    json_ret['end_time'] = end_time.seconds + end_time.nanos * 1e-9 
+                    json_arr.append(json_ret)
+                  
+        os.remove(real_name)
+
+        reduce((real_name,json_arr))
 
 
 def parseVideos(svideoLink:list,outputId):
@@ -70,55 +117,36 @@ def parseVideos(svideoLink:list,outputId):
         ydl.download(svideoLink)
 
 def transcribe_gcs():
-    global max_timestamp
-    client = speech.SpeechClient()
+    global currentReduceList
+    
     files = sorted(os.listdir('parts/'))
-    json_arr = []
+    threads = []
     for f in files:
         name = "parts/" + f
         if('mp3' in name):
-            extract(name)
-            real_name = name[0:len(name)-3] + ".flac"
-            os.remove(name)
-            with io.open(real_name, 'rb') as audio_file:
+            t = Thread(target=performWork, args=(name,))
+            threads.append(t)
+            t.start()
+    for i in threads:
+        i.join()
+    retCombinedList = []
+    previousTimeStamp = 0
+    for i in currentReduceList:
+        for j in i[1]:
+            j["start_time"]+=previousTimeStamp
+            j["end_time"]+=previousTimeStamp
+        previousTimeStamp = i[1][len(i[1])-1]["end_time"]
+        retCombinedList.extend(i[1])
+    print(retCombinedList)
+    print(len(retCombinedList))
+    
+    return retCombinedList
+
         
-                json_ret = {}
-                content = audio_file.read()
-
-                audio = types.RecognitionAudio(content=content)
-                config = types.RecognitionConfig(
-                     encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
-                        sample_rate_hertz=16000,
-                        language_code='en-US',
-                    enable_word_time_offsets=True
-                    )
-
-                response = client.recognize(config, audio)
-    # Each result is for a consecutive portion of the audio. Iterate through
-    # them to get the transcripts for the entire audio file.
-                for result in response.results:
-                    alternative = result.alternatives[0]
-        # The first alternative is the most likely one for this portion.
-                    print(u'Transcript: {}'.format(result.alternatives[0].transcript))   
-                    for i in range(len(alternative.words)):
-                         word_info = alternative.words[i]
-                         word = word_info.word
-                        
-                         start_time = word_info.start_time
-                         end_time = word_info.end_time
-                         json_ret['word'] = word
-                         json_ret['start_time'] = start_time.seconds + start_time.nanos * 1e-9 + max_timestamp
-                         json_ret['end_time'] = end_time.seconds + end_time.nanos * 1e-9 + max_timestamp
-                         json_arr.append(json_ret)
-                         if(i == len(alternative.words)-1):
-                             #update max
-                             max_timestamp = json_ret['end_time']
-            os.remove(real_name)
-
                     
 
 def splitAudio(bigAudio):
-    command =  "ffmpeg -i  " + bigAudio + " -f segment -segment_time 30 -c copy parts/out%03d.mp3"
+    command =  "ffmpeg -i  " + bigAudio + " -f segment -segment_time 59 -c copy parts/out%03d.mp3"
     subprocess.call(command,shell=True)
 
 def getVideosGivenPlayList(playListID:str):
@@ -151,5 +179,5 @@ def getVideosGivenPlayList(playListID:str):
 
 
 
-getVideosGivenPlayList("PLfMspJ0TLR5HRFu2kLh3U4mvStMO8QURm")
+getVideosGivenPlayList("PLD6cpMQHuQEQ-005myefm5J9oiXeBXRjJ")
 
